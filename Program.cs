97b1fdb8d72a00e25b1a3338ced6e4a7dea29404
Moustafa moungari <moungari.moustafa@gmail.com>
2022-05-12 16:01:09 +0200

    creation fonction demande longueur mot de passe

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace GenerateurMotDePasse
+{
+    class Program
+    {
+
+        static int DemanderNombre(string question)
+        {
+            bool isLongeurValid = false;
+            int longeurPwdMin = 5;
+            int iLongueurPwd = 0;
+            while (!isLongeurValid)
+            {
+
+                Console.WriteLine(question);
+                Console.WriteLine();
+                string longueurMotDePasse = Console.ReadLine();
+
+
+                try
+                {
+                    iLongueurPwd = int.Parse(longueurMotDePasse);
+                    if (iLongueurPwd == 0 || iLongueurPwd<=longeurPwdMin)
+                    {
+                        Console.WriteLine($"Mot de passe valide d'un minimum de {longeurPwdMin} caracteres");
+                        isLongeurValid = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Longueur Mot de passe valide de : {iLongueurPwd} caracteres");
+                        isLongeurValid = true;
+                    }
+                  
+
+
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Erreur vous n'avez pas saisie une valeur correct, seul les chiffres sont valides");
+                }
+
+
+            }
+
+
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            DemanderNombre("Indiquez la longueur du mot de passe souhaité :");
+        }
+    }
+}
