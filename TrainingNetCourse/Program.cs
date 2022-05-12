e310e79d01c140d9cd03a81c10dfd3a401ca5f12
Moustafa moungari <moungari.moustafa@gmail.com>
2022-05-12 10:32:11 +0200

    mise en place d'une fonction qui controle si le nom saisi n'est pas un chiffre

--- TrainingNetCourse/Program.cs
+++ TrainingNetCourse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Training_framework_4_8.Models;
 
 namespace premier_programme
@@ -7,13 +8,17 @@ namespace premier_programme
     {
 
 
+        static string regex = "\\D";
+        static bool isInvalidNom = false;
+
+
 
         static int DemanderAge(string nom)
         {
             int age_num = 0;
             while (age_num <= 0)
             {
-                Console.Write("votre nom est : "+ nom +" Quel est  l' age de la personne  ? ");
+                Console.Write("votre nom est : " + nom + " Quel est  l' age de la personne  ? ");
                 string age_str = Console.ReadLine();
 
                 try
@@ -39,26 +44,69 @@ namespace premier_programme
 
 
 
+        static bool isValidName(string nom)
+        {
+
+            bool match = Regex.IsMatch(nom, regex);
+
+            if (!match)
+            {
+
+                Console.WriteLine("Vous ne pouvez pas saisir de nombre");
+                isInvalidNom = true;
+
+            }
+            else
+            {
+                isInvalidNom = false;
+            }
+
+            return isInvalidNom;
+        }
+
+
+
         static string DemanderNom(int idPersonne)
         {
             // DEMANDE LE NOM
-            
+
 
             string nom = "";
-            while (nom == "")
+
+            while (nom == "" || isInvalidNom)
             {
-                Console.Write("quel est le nom  de la personne " + idPersonne +"?");
+                Console.Write("quel est le nom  de la personne " + idPersonne + "?");
                 //Console.Write("Quel est votre nom ? ");
+
                 nom = Console.ReadLine();
                 nom = nom.Trim();
+
+
+                // controle si le nom entré n'est pas un chiffre
+                isValidName(nom);
+
+
+
                 if (nom == "")
                 {
                     Console.WriteLine("Erreur: le nom ne doit pas être vide");
+
                 }
+
+
             }
 
             return nom;
 
+
+        }
+
+
+
+        static void AfficherResultat(string nom, int age)
+        {
+            Console.WriteLine("Bonjour, vous vous appelez " + nom + ", vous avez " + age + " ans");
+            Console.WriteLine("bientôt vous aurez " + (age + 1) + " ans");
 
         }
 
@@ -79,13 +127,19 @@ namespace premier_programme
             int age2 = DemanderAge(nom2);
 
             // AFFICHE LES RESULTATS
-            Console.WriteLine("Bonjour, vous vous appelez " + nom + ", vous avez " + age + " ans");
-            Console.WriteLine("Bonjour, vous vous appelez " + nom2+ ", vous avez " + age2 + " ans");
 
-            //int age_prochain = age + 1;
-            //int age_prochain2 = age2 + 1;
-            Console.WriteLine("bientôt vous aurez " + (age + 1) + " ans");
-            Console.WriteLine("bientôt vous aurez " + (age2 + 1) + " ans");
+
+            AfficherResultat(nom, age);
+            AfficherResultat(nom2, age2);
+
+
+            //Console.WriteLine("Bonjour, vous vous appelez " + nom + ", vous avez " + age + " ans");
+            //Console.WriteLine("Bonjour, vous vous appelez " + nom2+ ", vous avez " + age2 + " ans");
+
+            ////int age_prochain = age + 1;
+            ////int age_prochain2 = age2 + 1;
+            //Console.WriteLine("bientôt vous aurez " + (age + 1) + " ans");
+            //Console.WriteLine("bientôt vous aurez " + (age2 + 1) + " ans");
 
 
 
