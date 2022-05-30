660d903a867248628b83bdd5eeaa52dae86703e2
Moustafa moungari <moungari.moustafa@gmail.com>
2022-05-30 17:33:19 +0200

    ajout outils

---
+++ Outils.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerarteurMotDePasse
+{
+     static class Outils
+    {
+        /// <summary>
+        /// Verification sur la valeur saisi n'est pas 0 ou null
+        /// </summary>
+        /// <param name="question"></param>
+        /// <returns></returns>
+        public static int? DemanderNombrePositifNotNull(String question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue);
+        }
+
+
+
+        /// <summary>
+        /// Function recursive qui permet egalement de boucler si le resultat ne correspond pas .
+        /// </summary>
+        /// <param name="question"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int DemanderNombreEntre(string question, int min, int max)
+        {
+            int iMin = 5;
+            int iMax = 10;
+            int valeurSaisie = DemanderNombre(question);
+
+
+
+            if (valeurSaisie >= iMin && valeurSaisie <= iMax)
+            {
+
+                return valeurSaisie;
+            }
+
+            Console.WriteLine($"Vous devez entrer une valeur comprise entre {iMin} et {iMax}");
+            return DemanderNombreEntre(question, min, max);
+
+
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="question"></param>
+        /// <returns></returns>
+        public static int DemanderNombre(string question)
+        {
+         
+
+            int iLongueurPwd = 0;
+
+
+            while (true)
+            {
+
+                Console.WriteLine(question);
+                Console.WriteLine();
+                string longueurMotDePasse = Console.ReadLine();
+
+
+                try
+                {
+                    iLongueurPwd = int.Parse(longueurMotDePasse);
+
+
+
+                    return iLongueurPwd;
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Erreur vous n'avez pas saisie une valeur correct, seul les chiffres sont valides");
+                }
+
+
+            }
+
+
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,96 +1,26 @@
-﻿using System;
+﻿using GenerarteurMotDePasse;
+using System;
+using static GenerarteurMotDePasse.Outils;
 
 namespace GenerateurMotDePasse
 {
     class Program
     {
-
-
-        /// <summary>
-        /// Function recursive qui permet egalement de boucler si le resultat ne correspond pas .
-        /// </summary>
-        /// <param name="question"></param>
-        /// <param name="min"></param>
-        /// <param name="max"></param>
-        /// <returns></returns>
-        static int DemanderNombreEntre(string question, int min, int max)
-        {
-            int iMin = 5;
-            int iMax = 10;
-           
-            int nombre = DemanderNombre(question);
-            if (nombre >= iMin && nombre <= iMax)
-            {
-                return nombre;
-            }
-            else
-            {
-                Console.WriteLine($"Vous devez entrer une valeur comprise entre {iMin} et {iMax}");
-
-            }
-
-            return DemanderNombreEntre(question,min,max);
-
-
-        }
-
-
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="question"></param>
-        /// <returns></returns>
-        static int DemanderNombre(string question)
-        {
-            bool isLongeurValid = false;
-            int longeurPwdMin = 5;
-            int iLongueurPwd = 0;
-
-
-            while (!isLongeurValid)
-            {
-
-                Console.WriteLine(question);
-                Console.WriteLine();
-                string longueurMotDePasse = Console.ReadLine();
-
-
-                try
-                {
-                    iLongueurPwd = int.Parse(longueurMotDePasse);
-                    //if (iLongueurPwd == 0 || iLongueurPwd <= longeurPwdMin)
-                    //{
-                    //    Console.WriteLine($"Mot de passe valide d'un minimum de {longeurPwdMin} caracteres");
-                    //    isLongeurValid = false;
-                    //}
-                    //else
-                    //{
-                    //    Console.WriteLine($"Longueur Mot de passe valide de : {iLongueurPwd} caracteres");
-                    //    isLongeurValid = true;
-                    //}
-
-
-
-                }
-                catch (Exception)
-                {
-
-                    Console.WriteLine("Erreur vous n'avez pas saisie une valeur correct, seul les chiffres sont valides");
-                }
-
-
-            }
-
-
-            return 0;
-        }
+       
 
 
         static void Main(string[] args)
         {
-            DemanderNombre("Indiquez la longueur du mot de passe souhaité :");
+            String question = "Indiquez la longueur du mot de passe souhaité :";
+
+            int valeurSaisie = Outils.DemanderNombre(question);
+          
+
+            int interValValeur = Outils.DemanderNombreEntre(question, valeurSaisie,valeurSaisie);
+
+            Console.WriteLine($"la valeur saisi est  :  {interValValeur}");
+            
+            
            
         }
     }
