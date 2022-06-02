d8f934fb24f8a7a302f08ebe3e3aa53b19a61b33
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-02 13:46:37 +0200

    mise a jour de la classe Personne

---
+++ PooApp/Personne.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooApp
+{
+    class Personne
+    {
+        static int nombreDePersonnes = 0;
+
+        public string nom;
+        int age;
+        string emploi;
+        int numeroPersonne;
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// appel d'un second constructeur avec le mots this
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="age"></param>
+        public Personne(String nom, int age) : this(nom,age, "(non definit)") 
+        { 
+        
+        }
+
+        public Personne(string nom, int age, string emploi = null)
+        {
+            this.nom = nom;
+            this.age = age;
+            this.emploi = emploi;
+
+            nombreDePersonnes++;
+
+            this.numeroPersonne = nombreDePersonnes;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("PERSONNE N°" + numeroPersonne);
+            Console.WriteLine("  NOM : " + nom);
+            Console.WriteLine("  AGE : " + age + " ans");
+            if (emploi == null)
+            {
+                Console.WriteLine("  Aucun emploi spécifié");
+            }
+            else
+            {
+                Console.WriteLine("  EMPLOI : " + emploi);
+            }
+        }
+
+        public static void AfficherNombreDePersonnes()
+        {
+            Console.WriteLine("Nombre total de personnes : " + nombreDePersonnes);
+        }
+
+
+        }
+    }
--- PooApp/Program.cs
+++ PooApp/Program.cs
@@ -10,77 +10,9 @@ namespace PooApp
         static void Main(string[] args)
         {
 
-
-            Console.WriteLine($"========LINQ TO Object============");
-            var listEtudiant = new List<Etudiant>();
-            listEtudiant.Add(new Etudiant() { Name = "Billy", Age = 34, Id = 1 });
-            listEtudiant.Add(new Etudiant() { Name = "Arnold", Age = 45, Id = 2 });
-            listEtudiant.Add(new Etudiant() { Name = "Albert", Age = 12, Id = 3 });
-            listEtudiant.Add(new Etudiant() { Name = "Suzy", Age = 23, Id = 4 });
-            listEtudiant.Add(new Etudiant() { Name = "Karine", Age = 46, Id = 5 });
-            listEtudiant.Add(new Etudiant() { Name = "Ahmed", Age = 45, Id = 6 });
-
-            IEnumerable<Etudiant> EtudiantMajeur = from person in listEtudiant
-                                                   where(person.Age > 18)
-                                                   orderby person.Name ascending
-                                                   select person;
-
-
-            IEnumerable<Etudiant> EtudiantCommenceParA = from person in listEtudiant
-                                                   where (person.Name.StartsWith("A"))
-                                                   orderby person.Name ascending
-                                                   select person;
-
-            IEnumerable<Etudiant> EtudiantAgeComprisEntre = from person in listEtudiant
-                                                         where (person.Age >30 && person.Age<=45)
-                                                         orderby person.Name ascending
-                                                         select person;
-
-
-            var listEtudiantAgeSup30EtInf45 = listEtudiant.Where(e => e.Age >30 && e.Age<=45)
-                .OrderByDescending(e => e.Age).ToList();
-
-
-            
-
-
-
-            foreach (var item in EtudiantMajeur)
-            {
-                Console.WriteLine($"les etudiants majeurs sont : {item.Name} :  {item.Id}");
-            }
-
-            Console.WriteLine();
-
-            foreach (var item in EtudiantCommenceParA)
-            {
-                Console.WriteLine($"les etudiants dont le prenom commence par A : {item.Name} :  {item.Id}");
-            }
-
-
-            Console.WriteLine();
-            foreach (var item in listEtudiantAgeSup30EtInf45)
-            {
-                Console.WriteLine($"les etudiants age compris entre [30 et 45]: {item.Name} , AGE:  {item.Age}");
-            }
-
-            Console.WriteLine($"========LINQ TO XML============");
-
-            // lecture d'une fichier XML avec Linq
-            XElement xmldoc = XElement.Load("C:\\Users\\moungamo\\source\\repos\\PooApp\\PooApp\\contact.xml");
-            IEnumerable<XElement> contacts = xmldoc.Elements("contact");
-
-            var req1 = from c in contacts
-                       where Int32.Parse(c.Element("age").Value)>40
-                       orderby c.Attribute("id").Value ascending
-                       select c;
-
-            foreach (var item in req1)
-            {
-                Console.WriteLine($"ID {item.Attribute("id").Value}");
-                Console.WriteLine($"nom {item.Element("nom").Value}");
-                Console.WriteLine($"Age {item.Element("age").Value}");
-            }
+            var personne1 = new Personne("Albert", 30);
+            personne1.Afficher();
+         
 
 
         }
