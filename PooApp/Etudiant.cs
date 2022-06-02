dae40cbcde61a9cb3c23d036749d7fcf27d7453e
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-02 10:01:06 +0200

    Creation de plusieurs requete linq

---
+++ PooApp/Etudiant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooApp
+{
+    public  class Etudiant
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int Id { get; set; }
+
+
+
+    }
+}
